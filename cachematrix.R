8540856c80eeb1fd145b63cf9003536208c86f4d
Todd Simkin <todd.simkin@gmail.com>
2015-01-18 15:51:52 -0500

    Update cachematrix.R
    Update with my solution to HW#2 for rprog-010

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function "makeCacheMatrix" will create a list that will track
+## if an inverse has been calculated and the functions necessary to
+## calculate the inverse.  The function "cacheSolve" will retrieve the
+## cached solution if it exists, or calculate the inverse if it does not.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set=set, get=get, setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("Alread calculated -- getting cached data...")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
